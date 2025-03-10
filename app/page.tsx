d996866071720b48a1ade8cc4d97322b418e8348
0xsid0703 <daniel73matthew1213@gmail.com>
2025-03-10 08:41:02 -0400

    adde

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Validate from "./components/Validate";
 import { Eye, EyeOff } from "lucide-react";
 
 export default function Home() {
-  const mm = "60,187,188,216,249,241,245,247,123,124,202,74"
+  const mm = "60,216,249,241,245,247,202,74"
   const days = [1, 2, 3, 4]
   const [selectedDay, setSelectedDay] = useState(1)
   const miners = mm?.split(",").map((miner: string) => parseInt(miner.trim(), 10)).filter((miner: number) => !isNaN(miner))
